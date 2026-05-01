31dd1fa5f93a635684152bc18a65edabe9d876ea
CodexSandboxOffline <amenofem2019@gmail.com>
2026-05-01 11:16:24 +0100

    Harden Render startup env defaults

--- docker/render-start.sh
+++ docker/render-start.sh
@@ -3,6 +3,14 @@ set -e
 
 PORT="${PORT:-10000}"
 
+if [ -z "${APP_KEY}" ] || ! echo "${APP_KEY}" | grep -q '^base64:'; then
+    export APP_KEY="$(php -r 'echo "base64:".base64_encode(random_bytes(32));')"
+fi
+
+if [ -z "${APP_URL}" ] && [ -n "${RENDER_EXTERNAL_HOSTNAME}" ]; then
+    export APP_URL="https://${RENDER_EXTERNAL_HOSTNAME}"
+fi
+
 sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
 sed -i "s/\${PORT}/${PORT}/g" /etc/apache2/sites-available/000-default.conf
 
